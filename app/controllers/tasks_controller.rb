c04a4b77ccc37723d23122c7b28047554e3773a6
tabata.shougo <tbt12321@gmail.com>
2019-06-22 06:19:49 +0000

    CRUD task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,9 +46,10 @@ class TasksController < ApplicationController
   flash[:success] = 'タスクが削除されました'
   redirect_to tasks_path
   end
-end
-private
+  private
 
-def task_params
+  def task_params
   params.require(:task).permit(:content)
+  end
+
 end
